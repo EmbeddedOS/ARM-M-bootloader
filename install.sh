0a2da083d8a244b3571390210eeb80f9d7694792
EmbeddedOS <nguyenthanhcong12gtm1@gmail.com>
2024-10-28 11:00:48 -0700

    Add install toolchain script

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+wget https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
+
+tar -xvf arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
+
+mv arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi/ toolchain
+
+rm arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
