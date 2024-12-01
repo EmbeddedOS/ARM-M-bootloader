1543e02cfa9d587d7f335588001f9d23c3afe23b
EmbeddedOS <nguyenthanhcong12gtm1@gmail.com>
2024-12-01 07:23:20 -0800

    Add regiter accessor structures

--- drivers/inc/stm32f407_arch.h
+++ drivers/inc/stm32f407_arch.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#define __reg (volatile uint32_t *)
-#define __reg_t (volatile uint32_t)
-
 #define __pack __attribute__((packed, aligned(1)))
+
 /* Public types --------------------------------------------------------------*/
 typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
+
+#define __reg_t volatile uint32_t
+#define __reg (volatile uint32_t *)
 
 /* Memories's base addresses -------------------------------------------------*/
 #define MEM_FLASH_BASEADDR 0x08000000U
@@ -29,7 +30,7 @@ typedef unsigned char uint8_t;
 #define PER_GPIOF_BASEADDR (BUS_AHB1_BASEADDR + 0x1400)
 #define PER_GPIOG_BASEADDR (BUS_AHB1_BASEADDR + 0x1800)
 #define PER_GPIOH_BASEADDR (BUS_AHB1_BASEADDR + 0x1C00)
-#define PER_GPIOJ_BASEADDR (BUS_AHB1_BASEADDR + 0x2000)
+#define PER_GPIOI_BASEADDR (BUS_AHB1_BASEADDR + 0x2000)
 
 /* APB1 Bus peripherals's base addresses -------------------------------------*/
 #define PER_USART2_BASEADDR (BUS_APB1_BASEADDR + 0x4400)
@@ -41,8 +42,46 @@ typedef unsigned char uint8_t;
 #define PER_USART1_BASEADDR (BUS_APB2_BASEADDR + 0x1000)
 #define PER_USART6_BASEADDR (BUS_APB2_BASEADDR + 0x1400)
 
+/* Peripheral register accessors ---------------------------------------------*/
+#define REG_GPIOA ((gpio_reg_t *)PER_GPIOA_BASEADDR)
+#define REG_GPIOB ((gpio_reg_t *)PER_GPIOB_BASEADDR)
+#define REG_GPIOC ((gpio_reg_t *)PER_GPIOC_BASEADDR)
+#define REG_GPIOD ((gpio_reg_t *)PER_GPIOD_BASEADDR)
+#define REG_GPIOE ((gpio_reg_t *)PER_GPIOE_BASEADDR)
+#define REG_GPIOF ((gpio_reg_t *)PER_GPIOF_BASEADDR)
+#define REG_GPIOG ((gpio_reg_t *)PER_GPIOG_BASEADDR)
+#define REG_GPIOH ((gpio_reg_t *)PER_GPIOH_BASEADDR)
+#define REG_GPIOI ((gpio_reg_t *)PER_GPIOI_BASEADDR)
+
+#define REG_USART1 ((usart_reg_t *)PER_USART1_BASEADDR)
+#define REG_USART2 ((usart_reg_t *)PER_USART2_BASEADDR)
+#define REG_USART3 ((usart_reg_t *)PER_USART3_BASEADDR)
+#define REG_UART4 ((usart_reg_t *)PER_UART4_BASEADDR)
+#define REG_UART5 ((usart_reg_t *)PER_UART5_BASEADDR)
+#define REG_USART6 ((usart_reg_t *)PER_USART6_BASEADDR)
+
 /* Peripheral registers's structures -----------------------------------------*/
 typedef struct __pack
 {
-
+    __reg_t moder;   /* GPIO port mode register.                 */
+    __reg_t otyper;  /* GPIO port output type register.          */
+    __reg_t ospeedr; /* GPIO port output speed register.         */
+    __reg_t pupdr;   /* GPIO port pull-up/pull-down register.    */
+    __reg_t idr;     /* GPIO port input data register.           */
+    __reg_t odr;     /* GPIO port output data register.          */
+    __reg_t bsrr;    /* GPIO port bit set/reset register.        */
+    __reg_t lckr;    /* GPIO port configuration lock register.   */
+    __reg_t afrl;    /* GPIO alternate function low register.    */
+    __reg_t afrh;    /* GPIO alternate function high register.   */
 } gpio_reg_t;
+
+typedef struct __pack
+{
+    __reg_t sr;   /* Status register.                   */
+    __reg_t dr;   /* Data register.                     */
+    __reg_t brr;  /* Baud rate register.                */
+    __reg_t cr1;  /* Control register 1.                */
+    __reg_t cr2;  /* Control register 2.                */
+    __reg_t cr3;  /* Control register 3.                */
+    __reg_t gtpr; /* Guard time and prescaler register. */
+} usart_reg_t;
