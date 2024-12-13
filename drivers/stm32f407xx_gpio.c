6e4a081dd3eb796457f9555eeec3409c7572add6
Cong Nguyen <72590623+EmbeddedOS@users.noreply.github.com>
2024-12-13 18:57:13 +0700

    Update and rename gpio.c to stm32f407xx_gpio.c

---
+++ drivers/stm32f407xx_gpio.c
@@ -0,0 +1,37 @@
+#include <stm32f407xx_gpio.h>
+
+/* Public function -----------------------------------------------------------*/
+int __gpio_init(gpio_reg_t *reg, const gpio_pin_config_t *config)
+{
+    int res = 0;
+
+    if (reg == NULL || config == NULL)
+    {
+        res = -EINVAL;
+    }
+
+    if (config->mode == GPIO_MODE_INPUT)
+    {
+        
+    }
+
+    return res;
+}
+
+int gpio_read_pin(gpio_reg_t *reg, uint8_t pin, uint8_t *val)
+{
+
+    int res = 0;
+
+    return res;
+}
+
+int gpio_write_pin(gpio_reg_t *reg, uint8_t pin, uint8_t val)
+{
+
+}
+
+int gpio_config_deinit(gpio_reg_t *reg, uint8_t pin)
+{
+
+}
